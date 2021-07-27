91f7dd9ef8c110db026a9643d26a5f9d67b74703
Richard Coucoules <richardcoucoules@gmail.com>
2021-07-26 19:14:30 -0700

    Add Login and Signup pages

--- src/frontend/src/screens/Login.js
+++ src/frontend/src/screens/Login.js
@@ -1,5 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  return <div>Log In</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitLogin = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("backend/api/v1/login", {
+      method: "POST",
+      body: { email, password },
+    });
+
+    console.log(response.status);
+  };
+
+  return (
+    <div class="bg-gray-50 h-screen flex justify-center items-center text-center">
+      <div class="md:px-16 py-24 mx-auto bg-green-900 bg-opacity-25 rounded-sm w-5/6 max-w-screen-lg">
+        <h1 class="text-4xl">
+          Log in to <span class="text-green-900">Eden</span>
+        </h1>
+        <form
+          id="login"
+          onSubmit={(e) => submitLogin(e)}
+          class="mt-10 px-16 space-y-8"
+        >
+          <div class="flex flex-col">
+            <label for="email" class="self-start text-xl">
+              Email:
+            </label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              class="mt-1 pl-2 h-10 self-stretch bg-gray-50"
+            />
+          </div>
+          <div class="flex flex-col">
+            <label for="password" class="self-start text-xl">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              class="mt-1 pl-2 h-10 self-stretch bg-gray-50"
+            />
+          </div>
+          <div class="flex justify-around pt-8">
+            <input
+              type="submit"
+              id="submit"
+              value="Log In"
+              class="px-5 py-3 text-xl bg-gray-50 rounded-sm cursor-pointer hover:bg-green-900 hover:text-gray-50 hover:shadow-md"
+            />
+            <Link
+              to="/signup"
+              class="px-5 py-3 text-xl bg-gray-50 rounded-sm hover:bg-green-900 hover:text-gray-50 hover:shadow-md"
+            >
+              Sign up
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/frontend/src/screens/Signup.js
+++ src/frontend/src/screens/Signup.js
@@ -1,5 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
-  return <div>Sign Up</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const submitSignup = async (e) => {
+    e.preventDefault();
+    if (password === confirmPassword) {
+      if (password.length < 8) {
+        setErrorMessage("Password must be at least 8 characters.");
+      } else {
+        setErrorMessage("");
+        const response = await fetch("backend/api/v1/signup", {
+          method: "POST",
+          body: { email, password },
+        });
+        console.log(response.status);
+      }
+    } else {
+      setErrorMessage("Password and confirmation password must match.");
+    }
+  };
+
+  return (
+    <div class="bg-gray-50 h-screen flex justify-center items-center text-center">
+      <div class="md:px-16 py-24 mx-auto bg-green-900 bg-opacity-25 rounded-sm w-5/6 max-w-screen-lg">
+        <h1 class="text-4xl">
+          Sign up for <span class="text-green-900">Eden</span>
+        </h1>
+        <form
+          id="signup"
+          onSubmit={(e) => submitSignup(e)}
+          class="mt-10 px-16 space-y-8"
+        >
+          <div class="flex flex-col">
+            <label for="email" class="self-start text-xl">
+              Email:
+            </label>
+            <input
+              type="text"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              class="mt-1 pl-2 h-10 self-stretch bg-gray-50 rounded"
+            />
+          </div>
+          <div class="flex flex-col">
+            <label for="password" class="self-start text-xl">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              class="mt-1 pl-2 h-10 self-stretch bg-gray-50 rounded"
+            />
+          </div>
+          <div class="flex flex-col">
+            <label
+              for="password"
+              className={
+                "self-start text-xl" + (!errorMessage ? "" : " text-red-600")
+              }
+            >
+              Confirm Password:
+            </label>
+            <input
+              type="password"
+              id="confirm-password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className={
+                "mt-1 pl-2 h-10 self-stretch bg-gray-50 rounded" +
+                (!errorMessage ? "" : " border-2 border-red-600")
+              }
+            />
+            <p
+              className={
+                "mt-2 text-left text-red-600" + (!errorMessage ? " hidden" : "")
+              }
+            >
+              {errorMessage}
+            </p>
+          </div>
+          <div class="flex justify-around pt-8">
+            <input
+              type="submit"
+              id="submit"
+              value="Sign Up"
+              class="px-5 py-3 text-xl bg-gray-50 rounded-sm cursor-pointer hover:bg-green-900 hover:text-gray-50 hover:shadow-md"
+            />
+            <Link
+              to="/login"
+              class="px-5 py-3 text-xl bg-gray-50 rounded-sm hover:bg-green-900 hover:text-gray-50 hover:shadow-md"
+            >
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
