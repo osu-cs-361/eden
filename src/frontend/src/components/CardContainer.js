414a2550f0f879bf8909c90e643e303fabe05c89
Richard Coucoules <richardcoucoules@gmail.com>
2021-08-02 21:36:17 -0700

    Use responsive grid for cards

--- src/frontend/src/components/CardContainer.js
+++ src/frontend/src/components/CardContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CardContainer({ children }) {
   return (
-    <div className="grid grid-flow-row grid-cols-2 gap-3 md:grid-cols-4">
+    <div className="grid grid-flow-row grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
       {children}
     </div>
   );
