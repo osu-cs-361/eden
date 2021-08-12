dddf85c9d165ae141ef2026369b9df67b11d8f96
Richard Coucoules <richardcoucoules@gmail.com>
2021-08-12 11:42:13 -0700

    Use flexbox to display cards

--- src/frontend/src/components/CardContainer.js
+++ src/frontend/src/components/CardContainer.js
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function CardContainer({ children }) {
-  return (
-    <div className="grid grid-flow-row grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-      {children}
-    </div>
-  );
+  return <div className="flex gap-x-3 gap-y-4">{children}</div>;
 }
