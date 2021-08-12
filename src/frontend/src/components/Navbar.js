720f351718a6496b5571a2fe0e6eb715f0eac1b9
Richard Coucoules <richardcoucoules@gmail.com>
2021-08-12 11:10:15 -0700

    Bugfix: display navbar if no navLinks are passed

--- src/frontend/src/components/Navbar.js
+++ src/frontend/src/components/Navbar.js
@@ -6,13 +6,14 @@ export default function Navbar({ navLinks }) {
     <nav className="flex items-center justify-between w-full p-3 bg-green-900 h-1/12 text-gray-50">
       <span className="text-xl">Eden</span>
       <div className="flex gap-x-4">
-        {navLinks.map((link) => {
-          return (
-            <Link to={link.ref} key={link.ref}>
-              {link.text}
-            </Link>
-          );
-        })}
+        {navLinks &&
+          navLinks.map((link) => {
+            return (
+              <Link to={link.ref} key={link.ref}>
+                {link.text}
+              </Link>
+            );
+          })}
         <Link to="/logout">Log Out</Link>
       </div>
     </nav>
